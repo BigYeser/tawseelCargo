2a7facad56a29b5523c73a0e2287a15f104664cf
Abd Albary Tarakji <abd.tarakji96@gmail.com>
2022-12-04 14:44:08 +0400

    change password database

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 define("CDP_DB_HOST", "localhost:3306");
 define("CDP_DB_NAME", "deprixapro_database");
 define("CDP_DB_USER", "rootmaher");
-define("CDP_DB_PASS", "RootMaher100%");
+define("CDP_DB_PASS", "Xk9e$1b82");
 define('CDP_APP_URL', '');
 ?>
