c3f81597f29e4d30dc249b1e62a2b4ea20881586
Abd Albary Tarakji <abd.tarakji96@gmail.com>
2022-12-07 16:57:28 +0400

    fix wieght error entry

--- ajax/courier/get_price_range_weight_tariffs_ajax.php
+++ ajax/courier/get_price_range_weight_tariffs_ajax.php
@@ -100,7 +100,7 @@ if (empty($error)) {
                     $error = "The weight $calculate_weight is above the maximum required $max_final_range";
                 } else if ($calculate_weight < $min_initial_range) {
                     $error = "The weight $calculate_weight is below the minimum required $min_initial_range"
-                            ." please if your shipment weight less than $min_initial_range please enter $min_initial_range in weight field";
+                            .",      please if your shipment weight less than $min_initial_range please enter $min_initial_range in weight field";
                 }
             } else {
                 $error = "No hay tarifa configurada para";
