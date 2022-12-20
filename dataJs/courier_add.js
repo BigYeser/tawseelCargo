aa159c134fa8d30132d38370d8b0f224bd934cba
Abd Albary Tarakji <abd.tarakji96@gmail.com>
2022-12-20 16:11:13 +0400

    add items

--- dataJs/courier_add.js
+++ dataJs/courier_add.js
@@ -485,6 +485,7 @@ function changeItem(e) {
                 }
                 var qty = $('#itemQty_'+index).val();               
                 var id = $('#item_name_'+index).val();
+                console.log(id);
                 var price = items_type[index].item[id - 1].price;
                 console.log(price);
                 $('#total_' + index).val(qty * price);
