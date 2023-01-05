f7089867eac510828b2b24bb8b5f7c83bf8720d5
abdulsalam alkhateeb <abdulsalam@matched.se>
2023-01-05 20:37:42 +0200

    convert http to https

--- twilio_test.php
+++ twilio_test.php
@@ -3,7 +3,7 @@ require_once 'helpers/src/src/Twilio/autoload.php';
 use Twilio\Rest\Client;
 
 $sid    = "AC4021a82e9303879805e55b083a2e2615"; 
-$token  = "[cca094e1558954478162e571affe20f8]"; 
+$token  = "cca094e1558954478162e571affe20f8"; 
 $twilio = new Client($sid, $token); 
  
 $message = $twilio->messages 
