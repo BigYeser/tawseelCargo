9113dfe34247901e7fb6e2a932a218d9bd1f4fc8
abdulsalam alkhateeb <abdulsalam@matched.se>
2023-01-05 20:36:45 +0200

    convert http to https

--- twilio_test.php
+++ twilio_test.php
@@ -2,18 +2,16 @@
 require_once 'helpers/src/src/Twilio/autoload.php';
 use Twilio\Rest\Client;
 
-    $phone_sender = "+971501743345";
-    $sid    = "AC4021a82e9303879805e55b083a2e2615";
-    $token  = "f734ab8e186cd07eeda02e3af27e13d4";
-        $twilio = new Client($sid, $token);
-        $message = $twilio->messages
-            ->create(
-                "whatsapp:" . $phone_sender, // to 
-                array(
-                    "from" => "whatsapp:+971558982170",
-                    "body" => "a new shipment has been registered, *Tracking #$fullshipment* Follow up on your package by entering the following link and you will have detailed information on the status of your packages "
-                )
-            );
-        // echo '<script>console.log("message");</script>';
-        // echo '<script>console.log("'.$message.'");</script>';
-   
+$sid    = "AC4021a82e9303879805e55b083a2e2615"; 
+$token  = "[cca094e1558954478162e571affe20f8]"; 
+$twilio = new Client($sid, $token); 
+ 
+$message = $twilio->messages 
+                  ->create("whatsapp:+971501743345", // to 
+                           array( 
+                               "from" => "whatsapp:+14155238886",       
+                               "body" => "Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/" 
+                           ) 
+                  ); 
+ 
+print($message->sid);
